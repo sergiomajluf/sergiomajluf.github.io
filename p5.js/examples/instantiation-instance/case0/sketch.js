9f7cb493f58769a9ace993df5b65f01dae613d14
Sergio Majluf <sergio.majluf@gmail.com>
2014-03-31 15:54:14 -0400

    Added rect()

--- p5.js/examples/instantiation-instance/case0/sketch.js
+++ p5.js/examples/instantiation-instance/case0/sketch.js
@@ -1,21 +1,21 @@
 // CASE 0: no node specified
 // Canvas is auto-generated and appended to body.
-var s = function( sketch ) {
+var s = function(sketch) {
 
-  var gray = 0; 
+    var gray = 0;
 
-  sketch.setup = function() {
-    sketch.createCanvas(400, 400);
-  };
+    sketch.setup = function() {
+        sketch.createCanvas(400, 400);
+    };
 
-  sketch.draw = function() {
-    sketch.background(gray);
-    sketch.rect(sketch.width/2, sketch.height/2, 50, 50);
-  }
+    sketch.draw = function() {
+        sketch.background(gray);
+        sketch.rect(sketch.width / 2, sketch.height / 2, 50, 50);
+    }
 
-  sketch.mousePressed = function() {
-    gray += 10;
-  }
+    sketch.mousePressed = function() {
+        gray += 10;
+    }
 
 };
 
--- p5.js/examples/instantiation-instance/case1/sketch.js
+++ p5.js/examples/instantiation-instance/case1/sketch.js
@@ -1,21 +1,21 @@
 // CASE 1: node specified, node is a CANVAS ELEMENT ID
 // p5 will attach to canvas node specified.
-var s = function( sketch ) {
+var s = function(sketch) {
 
-  var gray = 0; 
+    var gray = 0;
 
-  sketch.setup = function() {
-    sketch.createCanvas(400, 400);
-  };
+    sketch.setup = function() {
+        sketch.createCanvas(400, 400);
+    };
 
-  sketch.draw = function() {
-    sketch.background(gray);
-    sketch.rect(sketch.width/2, sketch.height/2, 50, 50);
-  }
+    sketch.draw = function() {
+        sketch.background(gray);
+        sketch.rect(sketch.width / 2, sketch.height / 2, 50, 50);
+    }
 
-  sketch.mousePressed = function() {
-    gray += 10;
-  }
+    sketch.mousePressed = function() {
+        gray += 10;
+    }
 
 };
 
